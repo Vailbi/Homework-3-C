1764eedf23b7ba278f4da8409ab49ed3351cb9a3
Fedor <vailbi94@gmail.com>
2022-06-19 23:18:03 +0300

    new task 19

--- Задача 19/Program.cs
+++ Задача 19/Program.cs
@@ -5,7 +5,19 @@
  */
 
 Console.WriteLine("Введите число");
-string number1 = Console.ReadLine();
+int number = Convert.ToInt32(Console.ReadLine());
+int number2 = number/10;
+if (number/10000 == number%10 && (number/1000)%10==number2%10)
+{
+    Console.WriteLine("да");
+}
+
+else
+{
+    Console.WriteLine("нет");
+   
+}
+/* string number1 = Console.ReadLine();
 
 
 if (number1[0]==number1[4] && number1[1]==number1[3])
@@ -17,6 +29,6 @@ else
     Console.WriteLine("Нет");
 }
 
-
+ */
  
 
